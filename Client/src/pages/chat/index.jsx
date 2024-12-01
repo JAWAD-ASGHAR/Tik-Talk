9a6b5eccb56e3e887e8cc0b6e41024a8c8d66f25
JAWAD-ASGHAR <jawadasghar263@gmail.com>
2024-12-02 01:45:16 +0500

    added chat page layout and styling

--- Client/src/pages/chat/index.jsx
+++ Client/src/pages/chat/index.jsx
@@ -1,9 +1,10 @@
-// import React from 'react'
-
 import { useAppStore } from "@/store";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import ContactsContainer from "./components/contacts-container";
+import EmptyChatContainer from "./components/empty-chat-container";
+import ChatContainer from "./components/chats-container";
 
 const Chat = () => {
   const { userInfo } = useAppStore();
@@ -15,7 +16,11 @@ const Chat = () => {
     }
   }, [userInfo, navigate]);
 
-  return <div>Chat</div>;
+  return <div className="flex h-[100vh] text-white overflow-hidden">
+    <ContactsContainer />
+    {/* <EmptyChatContainer /> */}
+    <ChatContainer />
+  </div>;
 };
 
 export default Chat;
